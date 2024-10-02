9e9daf83621c6edbb0c5cf47c7d028ebc76d4d28
valeria-Wonsik <valeriawdss@gmail.com>
2024-10-02 20:04:06 -0300

    construindo array de frutas para estudo

--- aula.js
+++ aula.js
@@ -11,3 +11,10 @@ function subtração (A, B){
 
 soma (A, B)
 subtração (A, B)
+
+
+const frutas = ["maca", "manga" ,"morango", "melancia","maracuja", "acerola"];
+console.log("A fruta na posição 1 é:", frutas[1]); 
+console.log("A fruta na posição 1 é:", frutas[4]); 
+console.log("Meu array tem", frutas.length, "frutas");
+
