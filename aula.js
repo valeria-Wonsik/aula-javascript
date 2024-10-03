c4c997502882e79487cf814d98a0d46f22a6a995
valeria-Wonsik <valeriawdss@gmail.com>
2024-10-02 21:45:18 -0300

    novo

--- aula.js
+++ aula.js
@@ -17,4 +17,7 @@ const frutas = ["maca", "manga" ,"morango", "melancia","maracuja", "acerola"];
 console.log("A fruta na posição 1 é:", frutas[1]); 
 console.log("A fruta na posição 1 é:", frutas[4]); 
 console.log("Meu array tem", frutas.length, "frutas");
+console.log ("ultimo fruta", frutas[frutas])
+
+
 
